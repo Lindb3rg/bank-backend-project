321b8950687d5cc23072fa12e7a65ab4bfab68c1
lindberg <ad.lindberg@gmail.com>
2025-02-10 19:47:37 +0100

    removed-envfiles

---
+++ api/server.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	db "bank-backend-project/db/sqlc"
+	"bank-backend-project/utils"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+)
+
+type Server struct {
+	config utils.Config
+	store  db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and set up routing.
+func NewServer(config utils.Config, store db.Store) (*Server, error) {
+
+	server := &Server{
+		config: config,
+		store:  store,
+	}
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	server.router = router
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bank-backend-project/api"
+	db "bank-backend-project/db/sqlc"
+	"bank-backend-project/utils"
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 func main() {
 
-	fmt.Println("Hello world!")
+	config, err := utils.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(connPool)
+	server, err := api.NewServer(config, store)
+	err = server.Start(config.HTTPServerAddress)
 
 }
